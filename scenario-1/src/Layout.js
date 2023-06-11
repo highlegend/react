1b84f330e2768e4c8fd7b1a18e6d8e9fa009c98a
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-06-11 00:21:00 -0400

    Premier commit scenario 1

---
+++ scenario-1/src/Layout.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export default function Layout({ children }) {
+	return (
+		<>
+			<header className="header-app">
+				<nav className="nav nav-pills nav-fill gap-2 p-1 small bg-info rounded-5 shadow-sm">
+					<ul>
+						<li>
+							<Link to="/" style={{color: "black"}}>Home</Link>
+						</li>
+						<li>
+							<Link to="/products" style={{color: "black"}}>Products</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main>{children}</main>
+		</>
+	);
+}
